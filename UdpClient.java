c65fccde7f6799f8834e2632206a6822baf03765
Anjali <anjalitbaiju@gmail.com>
2018-04-03 14:38:19 +0530

    Client modified

--- UdpClient.java
+++ UdpClient.java
@@ -4,6 +4,8 @@ class UdpClient
 {
     public static void main(String args[]) throws Exception
    {
-    
+      BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+      DatagramSocket clientSocket = new DatagramSocket();
+      InetAddress IPAddress = InetAddress.getByName("localhost");
    }
 }
