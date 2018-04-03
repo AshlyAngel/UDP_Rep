48a94c1a15a605f074e96454ac26606331861aaa
ashly <elsyantonybabu@gmail.com>
2018-04-03 14:50:03 +0530

    server modified

--- UdpServer.java
+++ UdpServer.java
@@ -7,3 +7,7 @@ public static void main(String args[]) throws Exception
       DatagramSocket serverSocket = new DatagramSocket(4444);
             byte[] receiveData = new byte[1024];
             byte[] sendData = new byte[1024];
+            while(true)
+               {
+                  DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                  serverSocket.receive(receivePacket);
